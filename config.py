0cf48c1e1ecb6968825c4d8fba904aa2791b97a2
Jahongirshaxriddinov07 <shaxriddinovjahongir@gmail.com>
2026-09-18 12:42:24 +0500

    Add default values for environment variables
    Set default values for ADMIN_ID and STORAGE_CHANNEL_ID.

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID"))
+
+# ID larni xatosiz xavfsiz o'qib olish
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+STORAGE_CHANNEL_ID = int(os.getenv("STORAGE_CHANNEL_ID", 0))
+
 DATABASE_URL = os.getenv("DATABASE_URL")
